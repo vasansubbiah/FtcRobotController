017dc1a00c3f045d9f2293f10bdbc50f2a2f36cc
vasansubbiah <97911055+vasansubbiah@users.noreply.github.com>
2022-10-23 20:20:21 -0500

    teleop gamepad working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop2022/prototypesTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop2022/prototypesTeleop.java
@@ -6,28 +6,39 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.DMHardwareTest;
 
 @TeleOp(name="prototypesTeleop", group="prototypes")
-
 public class prototypesTeleop extends LinearOpMode {
     public DMHardwareTest robot = new DMHardwareTest(false);
     boolean bk =  true;
 
     @Override
     public void runOpMode() {
-        double y = gamepad1.left_stick_y;
-        double x = gamepad1.left_stick_x;
-        double rx = gamepad1.right_stick_x;
+        double y;
+        double x;
+        double rx;
 
         robot.initTeleOpIMU(hardwareMap, bk);
-
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            robot.frontLeft.setPower(y + x + rx);
-            robot.backLeft.setPower(y - x + rx);
-            robot.frontRight.setPower(y - x - rx);
-            robot.backRight.setPower(y + x - rx);
+            y = gamepad1.left_stick_y;
+            x = gamepad1.left_stick_x;
+            rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            robot.frontLeft.setPower(frontLeftPower/3.0);
+            robot.backLeft.setPower(backLeftPower/3.0);
+            robot.frontRight.setPower(frontRightPower/3.0);
+            robot.backRight.setPower(backRightPower/3.0);
         }
     }
 }
